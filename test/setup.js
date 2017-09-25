564055e6ded01afd751eb40d3cdba625e1deaf32
Shuwei Fang <sfang@homeaway.com>
2017-09-25 20:47:56 +0800

    Change to use mocha for testing

---
+++ test/setup.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Setup NODE_ENV for tests if not available
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'development';
+}
---
+++ test/unit/sample.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Sample', function () {
+    it('should pass', function () {
+        expect(2).to.equal(2);
+    });
+});
